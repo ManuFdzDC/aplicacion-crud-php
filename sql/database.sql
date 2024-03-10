3d70fea48cd91126b04c399057f801f9adcc432b
ManuFdzDC <manuel.fernandezdiazdecastro@iesmiguelherrero.com>
2024-03-10 20:34:40 +0100

    a

--- sql/database.sql
+++ sql/database.sql
@@ -1,16 +1,36 @@
-CREATE TABLE users (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  surname VARCHAR(100) NOT NULL,
-  name VARCHAR(100) NOT NULL,
-  age INT UNSIGNED NOT NULL
+CREATE SCHEMA IF NOT EXISTS crud_db;
+
+USE crud_db;
+
+CREATE TABLE cliente (
+  codigo_cliente INTEGER NOT NULL,
+  nombre_cliente VARCHAR(50) NOT NULL,
+  nombre_contacto VARCHAR(30) DEFAULT NULL,
+  apellido_contacto VARCHAR(30) DEFAULT NULL,
+  telefono VARCHAR(15) NOT NULL,
+  fax VARCHAR(15) NOT NULL,
+  linea_direccion1 VARCHAR(50) NOT NULL,
+  linea_direccion2 VARCHAR(50) DEFAULT NULL,
+  ciudad VARCHAR(50) NOT NULL,
+  region VARCHAR(50) DEFAULT NULL,
+  pais VARCHAR(50) DEFAULT NULL,
+  codigo_postal VARCHAR(10) DEFAULT NULL,
+  limite_credito NUMERIC(15,2) DEFAULT NULL,
+  PRIMARY KEY (codigo_cliente)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO users (surname, name, age) VALUES('Casas', 'Julian', 27);
-INSERT INTO users (surname, name, age) VALUES('Oro', 'Carlos', 32);
-INSERT INTO users (surname, name, age) VALUES('Valencia', 'Pablo', 29);
-INSERT INTO users (surname, name, age) VALUES('Dimas', 'Maria', 19);
-INSERT INTO users (surname, name, age) VALUES('Corrales', 'Marcos', 26);
-INSERT INTO users (surname, name, age) VALUES('Perez', 'Juan', 21);
-INSERT INTO users (surname, name, age) VALUES('Zapata', 'Pedro', 23);
+INSERT INTO cliente VALUES (1,'GoldFish Garden','Daniel G','GoldFish','5556901745','5556901746','False Street 52 2 A',NULL,'San Francisco',NULL,'USA','24006',3000);
+INSERT INTO cliente VALUES (3,'Gardening Associates','Anne','Wright','5557410345','5557410346','Wall-e Avenue',NULL,'Miami','Miami','USA','24010',6000);
+INSERT INTO cliente VALUES (4,'Gerudo Valley','Link','Flaute','5552323129','5552323128','Oaks Avenue nº22',NULL,'New York',NULL,'USA','85495',12000);
+INSERT INTO cliente VALUES (5,'Tendo Garden','Akane','Tendo','55591233210','55591233211','Null Street nº69',NULL,'Miami',NULL,'USA','696969',600000);
+INSERT INTO cliente VALUES (6,'Lasas S.A.','Antonio','Lasas','34916540145','34914851312','C/Leganes 15',NULL,'Fuenlabrada','Madrid','Spain','28945',154310);
+INSERT INTO cliente VALUES (7,'Beragua','Jose','Bermejo','654987321','916549872','C/pintor segundo','Getafe','Madrid','Madrid','Spain','28942',20000);
+INSERT INTO cliente VALUES (8,'Club Golf Puerta del hierro','Paco','Lopez','62456810','919535678','C/sinesio delgado','Madrid','Madrid','Madrid','Spain','28930',40000);
+INSERT INTO cliente VALUES (9,'Naturagua','Guillermo','Rengifo','689234750','916428956','C/majadahonda','Boadilla','Madrid','Madrid','Spain','28947',32000);
+INSERT INTO cliente VALUES (10,'DaraDistribuciones','David','Serrano','675598001','916421756','C/azores','Fuenlabrada','Madrid','Madrid','Spain','28946',50000);
+INSERT INTO cliente VALUES (11,'Madrileña de riegos','Jose','Tacaño','655983045','916689215','C/Lagañas','Fuenlabrada','Madrid','Madrid','Spain','28943',20000);
+INSERT INTO cliente VALUES (12,'Lasas S.A.','Antonio','Lasas','34916540145','34914851312','C/Leganes 15',NULL,'Fuenlabrada','Madrid','Spain','28945',154310);
+INSERT INTO cliente VALUES (13,'Camunas Jardines S.L.','Pedro','Camunas','34914873241','34914871541','C/Virgenes 45','C/Princesas 2 1ºB','San Lorenzo del Escorial','Madrid','Spain','28145',16481);
+
 
 
--- src/index.php
+++ src/index.php
@@ -19,14 +19,14 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<title>Panel de control</title>
-<!--	
+	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
--->	
+
 </head>
 <body>
-<!--	
+	
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--->	
+
 <div>
 	<header>
 		<h1>Panel de Control</h1>
@@ -37,36 +37,44 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 		<li><a href="index.php">Inicio</a></li>
 		<li><a href="add.html">Alta</a></li>
 	</ul>
-	<h2>Listado de trabajador@s</h2>
+	<h2>Listado de clientes</h2>
 	<table border="1">
 	<thead>
 		<tr>
-			<th>Nombre</th>
-			<th>Apellido</th>
-			<th>Edad</th>
-			<th>Acciones</th>
+			<th>Codigo de cliente</th>
+			<th>nombre de cliente</th>
+			<th>nombre de contacto</th>
+			<th>apellido de contacto</th>
+			<th>telefono</th>
+			<th>fax</th>
+			<th>linea_direccion1</th>
+			<th>linea_direccion2</th>
+			<th>ciudad</th>
+			<th>region</th>
+			<th>pais</th>
+			<th>codigo postal</th>
+			<th>limite de credito</th>
 		</tr>
 	</thead>
 	<tbdody>
 <?php
-/*mysqli_fetch_array- Busca una fila de una consulta y devuelve un array asociativo
-ARRAYS CON ÍNDICE
-$productos = array();
-$productos[0] = "Disco SSD";
-$productos[1] = "Memoria RAM";
-$productos[2] = "Monitor";
-ARRAYS ASOCIATIVO:
-$res["id"] = "7";
-$res["name"] = "Pedro";
-$res["surname"] = "Zapata";
-$res["age"] = "23";
-*/
+
 //Genera la tabla de la página inicial
 	while($res = mysqli_fetch_array($result)) {
 		echo "<tr>\n";
-		echo "<td>".$res['name']."</td>\n";
-		echo "<td>".$res['surname']."</td>\n";
-		echo "<td>".$res['age']."</td>\n";
+		echo "<td>".$res['codigo_cliente']."</td>\n";
+		echo "<td>".$res['nombre_cliente']."</td>\n";
+		echo "<td>".$res['nombre_contacto']."</td>\n";
+		echo "<td>".$res['apellido_contacto']."</td>\n";
+		echo "<td>".$res['telefono']."</td>\n";
+		echo "<td>".$res['fax']."</td>\n";
+		echo "<td>".$res['linea_direccion1']."</td>\n";
+		echo "<td>".$res['linea_direccion2']."</td>\n";
+		echo "<td>".$res['ciudad']."</td>\n";
+		echo "<td>".$res['region']."</td>\n";
+		echo "<td>".$res['pais']."</td>\n";
+		echo "<td>".$res['codigo_postal']."</td>\n";
+		echo "<td>".$res['limite_credito']."</td>\n";
 		echo "<td>";
 //En la última columna se añader dos enlaces para editar y modificar el registro correspondiente. Se le pasa por el método GET el id del registro		
 		echo "<a href=\"edit.php?id=$res[id]\">Editar</a>\n";
@@ -81,7 +89,7 @@ $res["age"] = "23";
 	</table>
 	</main>
 	<footer>
-    Created by the IES Miguel Herrero team &copy; 2024
+    Created by manufdzdc &copy; 2024
   	</footer>
 </div>
 </body>
